cb81caa9bb8399fb596f5fd93516fa91b527d9d6
Amit Kumar Singh <ak.singh7@samsung.com>
2010-09-28 12:10:38 +0530

    [AUI Initial Version]Added Haptic Support For AUI

--- src/modules/slp_sound_haptic/sound_haptic.c
+++ src/modules/slp_sound_haptic/sound_haptic.c
@@ -1,7 +1,20 @@
 #include"sound.h"
 #include"haptic.h"
 
-
+int dev_handle = -1 ;
+Eina_Bool aui_haptic_play(char* filename,int iteration)
+{
+   int ret_val=0 ;
+   int mode=0;
+   if(dev_handle!=-1)device_haptic_close(dev_handle) ;
+   dev_handle= device_haptic_open(1,mode);
+   if(dev_handle < 0)return EINA_FALSE ;
+   ret_val = device_haptic_play_file(dev_handle,filename,iteration,3);
+   if(ret_val<0)
+     return EINA_FALSE ;
+   return EINA_TRUE ;
+}	
+			 	
 EAPI Eina_Bool ems_ui_sound_play(Edje_Sound_Info* sinfo, Edje_Sound_Data* sdata, unsigned int it)
   {    
      int volume_level = 0 ;
@@ -30,10 +43,60 @@ EAPI Eina_Bool ems_ui_sound_play(Edje_Sound_Info* sinfo, Edje_Sound_Data* sdata,
     PLAY:
        mm_sound_play_keysound(buf,volume_level)  ;
      return EINA_TRUE ;
-  }
+   }
+int HexToDec(const char* strHex)
+{	
+   int nFactor = 1;
+   int nRetVal = 0;
+   char* p;
+   for(p = strHex + strlen(strHex); p >= strHex; --p)
+     {
+        if( p[0] == ' ' || p[0] == 0) continue ;		
+       	if(p[0] < 'A') nRetVal += nFactor * (p[0] - '0');
+	else if(p[0] < 'a') nRetVal += nFactor * (p[0] - 'A' + 10);											
+	else nRetVal += nFactor * (p[0] - 'a' + 10);												
+ 	nFactor *= 16;			
+     }
+	   
+   return nRetVal;
+}
 
 EAPI Eina_Bool ems_ui_haptic_play(Edje_Haptic_Info *haptic, unsigned int iterations)
    {
-	/*TO BE IMPLEMENTED*/	
+	char *pattern =NULL ;
+	pattern=(char*)malloc(strlen(haptic->pattern)) ;
+        char *t = NULL;
+        strcpy(pattern, haptic->pattern);
+	char *s =pattern ;
+        int a = 0;
+	char buf[256] ;
+	char test[3] ;
+	test[2]=0 ;
+	int i=0 ;
 
-    }
+	int counter = 0;
+	char data ;
+	snprintf(buf,sizeof(buf),"/tmp/%s.ivt",haptic->name) ;
+	FILE *f=fopen(buf,"w") ;
+	
+	 while (*s != '\0')
+	{
+           if (*s == ',')
+   	     counter++;
+	   s++;
+	}
+	
+	 while (a <= counter)
+	      {
+ 		 test[0]= pattern[i] ;
+	         test[1]=pattern[i+1] ;
+      	         i = i + 3 ;
+		 data=HexToDec(test) ;
+		 fputc(data,f) ;
+		 a++ ;
+	      }
+	     
+	     fclose(f) ;
+	     if(aui_haptic_play(buf,iterations)==EINA_TRUE) ;
+	     return EINA_TRUE    ;
+   }
