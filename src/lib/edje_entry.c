010d6a64faef8d86e784eba4eb50ed52e39f4c61
WooHyun Jung <wh0705.jung@samsung.com>
2011-03-20 17:04:28 +0900

    edje_entry : clear code

--- src/lib/edje_entry.c
+++ src/lib/edje_entry.c
@@ -2713,8 +2713,6 @@ _edje_entry_real_part_shutdown(Edje_Real_Part *rp)
    rp->edje->subobjs = eina_list_remove(rp->edje->subobjs, en->block_handler_btm);
    evas_object_del(en->cursor_bg);
    evas_object_del(en->cursor_fg);
-   evas_object_del(en->block_handler_top);
-   evas_object_del(en->block_handler_btm);
 
    if (en->block_handler_top)
 	   evas_object_del(en->block_handler_top);
